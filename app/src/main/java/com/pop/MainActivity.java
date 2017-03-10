c7dc325700091d735eeb121820ac74dce1ea060f
yangshirong <ysr200808@163.com>
2017-03-10 11:12:51 +0800

    Initial commit

---
+++ app/src/main/java/com/pop/MainActivity.java
@@ -0,0 +1,51 @@
+package com.pop;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private FloatingActionButton fab;
+    private BottomPopupOption bottomPopupOption;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+
+        fab.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.fab:
+
+                bottomPopupOption = new BottomPopupOption(MainActivity.this);
+                bottomPopupOption.setItemText("男", "女","无","智障");
+                // bottomPopupOption.setColors();//设置颜色
+                bottomPopupOption.showPopupWindow();
+                bottomPopupOption.setItemClickListener(new Mylistener());
+                break;
+        }
+    }
+
+    private class Mylistener implements BottomPopupOption.onPopupWindowItemClickListener {
+        @Override
+        public void onItemPositionClick(int position) {
+
+        }
+        @Override
+        public void onItemTextClick(String text) {
+            Toast.makeText(MainActivity.this,""+text,Toast.LENGTH_SHORT).show();
+            bottomPopupOption.dismiss();
+        }
+    }
+}
